71e4d0cda4ccd18571f09e69a4b9a61be2278888
J Hunt <github.com@huntdesign.co.nz>
2018-04-10 15:19:57 +1200

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -4,6 +4,8 @@ var cheerio = require("cheerio");
 var request = require("request");
 var sqlite3 = require("sqlite3").verbose();
 
+let api_key = process.env.MORPH_MYSECRET;
+
 function initDatabase(callback) {
 	// Set up sqlite database.
 	var db = new sqlite3.Database("data.sqlite");
@@ -41,9 +43,11 @@ function fetchPage(url, callback) {
 
 function run(db) {
 	// Use request to read in pages.
-	fetchPage("https://morph.io", function (body) {
+	fetchPage("https://api.digitalnz.org/records.json?and[dc_type]=Name+Authority&and[collection]=TAPUHI&record_type=1&fields=title,record_id&sort=syndication_date&direction=desc&api_key=" + api_key, function (body) {
 		// Use cheerio to find things in the page with css selectors.
-		var $ = cheerio.load(body);
+		//var $ = cheerio.load(body);
+		var results = body.search.results;
+		
 
 		var elements = $("div.media-body span.p-name").each(function () {
 			var value = $(this).text().trim();
