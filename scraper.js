94003af72513f243c929c9bab3b327502562b492
Jonathan Hunt <git@huntdesign.co.nz>
2018-04-17 17:53:47 +1200

    Strip tags. Add process.env.MORPH_API_KEY.

--- scraper.js
+++ scraper.js
@@ -7,57 +7,60 @@ var sqlite3 = require("sqlite3").verbose();
 let api_key = process.env.MORPH_MYSECRET;
 
 function initDatabase(callback) {
-	// Set up sqlite database.
-	var db = new sqlite3.Database("data.sqlite");
-	db.serialize(function() {
-		db.run("CREATE TABLE IF NOT EXISTS data (name TEXT)");
-		callback(db);
-	});
+  // Set up sqlite database.
+  var db = new sqlite3.Database("data.sqlite");
+  db.serialize(function() {
+    db.run("CREATE TABLE IF NOT EXISTS data (name TEXT)");
+    callback(db);
+  });
 }
 
 function updateRow(db, value) {
-	// Insert some data.
-	var statement = db.prepare("INSERT INTO data VALUES (?)");
-	statement.run(value);
-	statement.finalize();
+  // Insert some data.
+  var statement = db.prepare("INSERT INTO data VALUES (?)");
+  statement.run(value);
+  statement.finalize();
 }
 
 function readRows(db) {
-	// Read some data.
-	db.each("SELECT rowid AS id, name FROM data", function(err, row) {
-		console.log(row.id + ": " + row.name);
-	});
+  // Read some data.
+  db.each("SELECT rowid AS id, name FROM data", function(err, row) {
+    console.log(row.id + ": " + row.name);
+  });
 }
 
 function fetchPage(url, callback) {
-	// Use request to read in pages.
-	request(url, function (error, response, body) {
-		if (error) {
-			console.log("Error requesting page: " + error);
-			return;
-		}
+  // Use request to read in pages.
+  request(url, function (error, response, body) {
+    if (error) {
+      console.log("Error requesting page: " + error);
+      return;
+    }
 
-		callback(body);
-	});
+    callback(body);
+  });
 }
 
 function run(db) {
-	// Use request to read in pages.
-	fetchPage("https://api.digitalnz.org/records.json?and[dc_type]=Name+Authority&and[collection]=TAPUHI&record_type=1&fields=title,record_id&sort=syndication_date&direction=desc&api_key=" + api_key, function (body) {
-		// Use cheerio to find things in the page with css selectors.
-		//var $ = cheerio.load(body);
-		var results = body.search.results;
-		
+  // Use request to read in pages.
+  let api_key = process.env.MORPH_API_KEY;
+  fetchPage("https://api.digitalnz.org/records.json?and[dc_type]=Name+Authority&and[collection]=TAPUHI&record_type=1&fields=title,record_id&sort=syndication_date&direction=desc&api_key=" + api_key, function (data) {
+    // Use cheerio to find things in the page with css selectors.
+    //var $ = cheerio.load(body);
+    console.log("data=", data);
+/*
+    var results = body.search.results;
+    
 
-		var elements = $("div.media-body span.p-name").each(function () {
-			var value = $(this).text().trim();
-			updateRow(db, value);
-		});
+    var elements = $("div.media-body span.p-name").each(function () {
+      var value = $(this).text().trim();
+      updateRow(db, value);
+    });
 
-		readRows(db);
-
-		db.close();
-	});
+    readRows(db);
+*/
+    db.close();
+  });
 }
 
 initDatabase(run);
