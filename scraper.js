fde47394442c00d42b8e30eb30fb9c3755b28040
Jonathan Hunt <git@huntdesign.co.nz>
2018-05-02 17:47:06 +1200

    Read initial page of results and store in sqlite.

--- scraper.js
+++ scraper.js
@@ -1,4 +1,4 @@
-// This is a template for a Node.js scraper on morph.io (https://morph.io)
+// This is a template for a Node.js scraper on morph.io (https://morph.io).
 
 var cheerio = require("cheerio");
 var request = require("request");
@@ -46,23 +46,31 @@ function run(db) {
   //console.log("process.env", process.env);
   //console.log("process.env.MORPH_API_KEY=" + process.env.MORPH_API_KEY);
   var api_key = process.env.MORPH_API_KEY;
-  console.log("api_key defined?", api_key == null);
+  //console.log("process.argv", process.argv);
+  
+  // If no key, we may be running on local.
+  console.log("api_key defined?", api_key !== null);
   fetchPage("https://api.digitalnz.org/records.json?and[dc_type]=Name+Authority&and[collection]=TAPUHI&record_type=1&fields=title,record_id&sort=syndication_date&direction=desc&api_key=" + api_key, function (data) {
-    // Use cheerio to find things in the page with css selectors.
-    //var $ = cheerio.load(body);
-    console.log("data=", data.search.results);
-    var results = data.search.results;
-    var len = results.length;
-    console.log("len=", len);
-    
-/*
-    var elements = $("div.media-body span.p-name").each(function () {
-      var value = $(this).text().trim();
-      updateRow(db, value);
-    });
 
+    //console.log("data=", data);
+    let results = JSON.parse(data);
+    console.log("results=", results);
+    //var results = data.search.results;
+    var len = results.search.results.length;
+    console.log("len=", len);
+    if (len > 0) {
+      for(i=0;i<len;i++) {
+        var record = results.search.results[i];
+        if (record != null) {
+          var id = record.record_id;
+          var name = record.title;
+          console.log('record=', [id, name]);
+          updateRow(db, [id, name]);
+        }
+      }
+    }
     readRows(db);
-*/
+
     db.close();
   });
 }
